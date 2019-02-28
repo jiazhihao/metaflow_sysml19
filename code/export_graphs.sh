22cc76782d93a3f977cd3b663df5c8de7b0bd587
Todd Irving Warszawski <twarszaw@sh-ln07.stanford.edu>
2019-02-27 16:24:37 -0800

    Updating the export graphs script to match the camera ready version of the paper

--- code/export_graphs.sh
+++ code/export_graphs.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-./cnn_cuda --export squeezenet_raw --dnn squeezenet --noopt > squeezenet_raw_output
-./cnn_cuda --export inception_raw --dnn inception --noopt > inception_raw_output
-./cnn_cuda --export resnet34_raw --dnn resnet34 --noopt > resnet34_raw_output
-./cnn_cuda --export rnntc_raw --dnn rnntc --noopt > rnntc_raw_output
+../mf --export squeezenet_raw --dnn squeezenet --noopt > squeezenet_raw_output
+../mf --export inception_raw --dnn inception --noopt > inception_raw_output
+../mf --export resnet50_raw --dnn resnet50 --noopt > resnet50_raw_output
+../mf --export rnntc_raw --dnn rnntc --noopt > rnntc_raw_output
 
-./cnn_cuda --export squeezenet_opt --dnn squeezenet > squeezenet_opt_output
-./cnn_cuda --export inception_opt --dnn inception > inception_opt_output
-./cnn_cuda --export resnet34_opt --dnn resnet34 > resnet34_opt_output
-./cnn_cuda --export rnntc_opt --dnn rnntc > rnntc_opt_output
+../mf --export squeezenet_opt --dnn squeezenet > squeezenet_opt_output
+../mf --export inception_opt --dnn inception > inception_opt_output
+../mf --export resnet50_opt --dnn resnet50 > resnet50_opt_output
+../mf --export rnntc_opt --dnn rnntc > rnntc_opt_output
